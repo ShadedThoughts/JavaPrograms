ec5f203ee99e0e2b76a592fa6f54e073cf5e0bc9
ShadedThoughts <yokun4real@gmail.com>
2020-03-30 00:27:49 +0000

    Added an output

--- HelloWord.java
+++ HelloWord.java
@@ -2,7 +2,8 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 	// Prints "Hello, Java" in the terminal window.
-	System.out.println("Hello, World");
+	System.out.println("Hello, World")
+	System.out.println("Hurray!, COVID-19 ends");
 
 	}
 
