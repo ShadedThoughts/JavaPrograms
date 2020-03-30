eb3173077fca567847c444f6a295126f9115b734
ShadedThoughts <62673231+ShadedThoughts@users.noreply.github.com>
2020-03-30 01:45:10 +0100

    Adden comment line

--- HelloWord.java
+++ HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 	// Prints "Hello, Java" in the terminal window.
+	// Check with POC build triggers on Jenkins
 	System.out.println("Hello, World")
 	System.out.println("Hurray!, COVID-19 ends");
 
